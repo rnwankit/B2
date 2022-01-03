2c5390bb4b53b424aae4007e2b8cc9e3f1105a8a
rnwankit <88432326+rnwankit@users.noreply.github.com>
2022-01-03 07:57:11 +0530

    pr5

--- src/Containers/Medicines/Medicines.js
+++ src/Containers/Medicines/Medicines.js
@@ -1,58 +1,127 @@
-import React from 'react';
-import Lists from '../../Components/Common/List/List';
+import React, { useState } from 'react';
+import List from '../Components/Common/List/List';
+import InputBox from '../Components/Common/InputBox/InputBox'
+
+const orgData = [
+    {
+        id: 101,
+        name: 'Abacavir',
+        quantity: 25,
+        price: 150,
+        expiry: 2022
+    },
+    {
+        id: 102,
+        name: 'Eltrombopag',
+        quantity: 90,
+        price: 550,
+        expiry: 2021
+    },
+    {
+        id: 103,
+        name: 'Meloxicam',
+        quantity: 85,
+        price: 450,
+        expiry: 2025
+    },
+    {
+        id: 104,
+        name: 'Allopurinol',
+        quantity: 50,
+        price: 600,
+        expiry: 2023
+    },
+    {
+        id: 105,
+        name: 'Phenytoin',
+        quantity: 63,
+        price: 250,
+        expiry: 2021
+    }
+]
 
 function Medicines(props) {
-    const data = [
-        {
-            id: 101,
-            name: 'Abacavir',
-            quantity: 25,
-            price: 150,
-            expiry: 2022
-        },
-        {
-            id: 102,
-            name: 'Eltrombopag',
-            quantity: 90,
-            price: 550,
-            expiry: 2021
-        },
-        {
-            id: 103,
-            name: 'Meloxicam',
-            quantity: 85,
-            price: 450,
-            expiry: 2025
-        },
-        {
-            id: 104,
-            name: 'Allopurinol',
-            quantity: 50,
-            price: 600,
-            expiry: 2023
-        },
-        {
-            id: 105,
-            name: 'Phenytoin',
-            quantity: 63,
-            price: 250,
-            expiry: 2021
-        },
-    ]
+    const [filterData, setFilterData] = useState()
+    const [sortData, setSortData] = useState()
+    const [sort, setSort] = useState()
+
+    const handleSearch = (s) => {
+        if (s !== "") {
+            let fData = orgData.filter((d) => (
+                d.name.toString().toLowerCase().includes(s.toLowerCase())
+            ))
+    
+            setFilterData([...fData])
+        } else {
+            setFilterData()
+            handleSort(sort,"yes");
+        }
+        
+    }
+
+    const handleSort = (s="", empty='') => {
+        setSort(s)
+        let data = filterData && empty ==='' ? filterData : orgData
+        
+        if (s !== "0") {
+            let sData = data.sort((a, b) => {
+                if (s == "lh") {
+                    return a.price - b.price
+                } else if (s == "hl") {
+                    return b.price - a.price
+                } else if (s == "e") {
+                    return a.expiry - b.expiry
+                } else if (s == "q") {
+                    return a.quantity - b.quantity
+                }
+            })
+            setSortData([...sData])
+        }
+    }
+
+    let finalData = filterData ? filterData : sortData ? sortData : orgData
+
+    console.log("return");
+    
     return (
         <div>
-            <section id="appointment" class="appointment">
-                <div class="container">
-                    <div class="section-title">
-                        <h2>Medicines</h2>
+            <section id="about" className="about">
+                <div className="container">
+                    <div className="row d-flex justify-content-center">
+                        <div className="text-center col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center px-lg-5 abouttop">
+                            <h2>Medicines</h2>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div className='row'>
+                        <div className='col-6'>
+                            <InputBox
+                                placeholder="Search medicines..."
+                                onChange={(e) => handleSearch(e.target.value)}
+                            />
+                        </div>
+                        <div className='col-6'>
+                            <InputBox
+                                type="select"
+                                onChange={(e) => handleSort(e.target.value)}
+                            >
+                                <option value="0">--Select--</option>
+                                <option value="lh">Price: Low to High</option>
+                                <option value="hl">Price: High to Low</option>
+                                <option value="e">Expiry</option>
+                                <option value="q">Quantity</option>
+                            </InputBox>
+                        </div>
+                    </div>
+
+                    <div className='row'>
                         {
-                            data.map((d) => {
-                                return (
-                                    <Lists key={d.id.toString()} data={d} />
-                                )
-                            })
+                            finalData !== undefined ?
+                                finalData.map((d, index) => {
+                                    return (
+                                        <List key={d.id.toString()} data={d} />
+                                    )
+                                })
+                                : null
                         }
                     </div>
                 </div>
